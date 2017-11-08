50f64adc8fc93a51908b026ada82a47962d2a3f7
Synnro <Synnro@gmail.com>
2017-11-08 22:47:02 +1300

    Initial commit

---
+++ main.js
@@ -0,0 +1,65 @@
+const FileSystem = require("fs");
+const Discord = require("discord.js");
+const Logging = require("./src/logging.js");
+
+const bot = new Discord.Client();
+const logger = new Logging.Logger();
+
+function isGuild(id){
+  return false;
+}
+
+bot.on("message", (message)=>{
+  if(!isGuild(message.guild.id))
+    return;
+  return logger.logMessageActivity(message.author.id, message.channel.id);
+});
+
+bot.on("voiceStateUpdate", (previous, current)=>{
+  if(!isGuild(previous.guild.id))
+    return;
+
+  var user = previous.user;
+
+  // connect
+  if(!previous.voiceChannelID)
+    return logger.logVoiceActivity(user.id, current.voiceChannelID, Logging.Actions.CONNECT);
+
+  // disconnect
+  if(!current.voiceChannelID)
+    return logger.logVoiceActivity(user.id, previous.voiceChannelID, Logging.Actions.DISCONNECT);
+
+  // change room
+  if(previous.voiceChannelID != current.voiceChannelID)
+    return logger.logVoiceActivity(user.id, current.voiceChannelID, Logging.Actions.CHANGEROOM);
+
+  // deafen
+  if(!previous.selfDeaf && current.selfDeaf)
+    return logger.logVoiceActivity(user.id, current.voiceChannelID, Logging.Actions.DEAFEN);
+
+  // undeafen
+  if(previous.selfDeaf && !current.selfDeaf)
+    return logger.logVoiceActivity(user.id, current.voiceChannelID, Logging.Actions.UNDEAFEN);
+
+  // mute
+  if(!previous.selfMute && current.selfMute)
+    return logger.logVoiceActivity(user.id, current.voiceChannelID, Logging.Actions.MUTE);
+
+  // unmute
+  if(previous.selfMute && !current.selfMute)
+    return logger.logVoiceActivity(user.id, current.voiceChannelID, Logging.Actions.UNMUTE);
+});
+
+bot.on("ready", ()=>{
+})
+
+bot.on("reconnecting", ()=>{
+})
+
+FileSystem.readFile("./conf.json", "utf8", (error, data)=>{
+  if(error) throw error;
+  var conf = JSON.parse(data);
+  bot.login(conf.token);
+  logger.init("./data/" + conf.guild + ".db");
+  isGuild = (id)=>{return (id === conf.guild) }
+});
---
+++ src/logging.js
@@ -0,0 +1,88 @@
+const Sqlite3 = require("sqlite3").verbose();
+
+const Actions = {
+  CONNECT: 0,
+  DISCONNECT: 1,
+  CHANGEROOM: 2,
+  MUTE: 3,
+  UNMUTE: 4,
+  DEAFEN: 5,
+  UNDEAFEN: 6,
+  display : (i)=>{
+    return Object.keys(Actions)[i];
+  }
+}
+
+function seconds(){
+  return Date.now()/1000;
+}
+
+function sqltable( name, table ){
+  var sql = "CREATE TABLE IF NOT EXISTS " + name;
+  sql += "(id INTEGER PRIMARY KEY"
+  for(var key in table ){
+    sql += ", " + key + " " + table[key];
+  }
+  return sql + ")";
+}
+
+function Logger(){
+  this.database = null;
+  this.init = (path)=>{
+    // connect to db
+    var db = new Sqlite3.Database(path);
+    // init db structure
+    db.serialize(()=>{
+      db.run(sqltable("Users", {
+        userid : "INT",
+        username : "TEXT"
+      }))
+      db.run(sqltable("Channels", {
+        channelid : "INT",
+        channelname : "TEXT"
+      }))
+      db.run(sqltable("VoiceActivity", {
+        userid : "INT",
+        channelid : "INT",
+        timestamp : "INT",
+        action : "INT"
+      }))
+      db.run(sqltable("MessageActivity", {
+        userid : "INT",
+        channelid : "INT",
+        timestamp : "INT"
+      }))
+    });
+    this.database = db;
+  }
+  this.logUser = (userid, username)=>{
+    this.database.run("INSERT INTO Users(id, name) VALUES(?, ?)", [userid, username]);
+
+  }
+  this.logMessageActivity = (userid, channelid)=>{
+    console.log({
+      user : userid,
+      channel : channelid,
+      timestamp : seconds()
+    })
+
+
+    this.database.run("INSERT INTO MessageActivity(userid, channelid, timestamp) VALUES(?, ?, ?)",
+      [userid, channelid, seconds()]);
+  }
+  this.logVoiceActivity = (userid, channelid, action)=>{
+    console.log({
+      user : userid,
+      channel : channelid,
+      timestamp : seconds(),
+      action : Actions.display(action)
+    })
+    this.database.run("INSERT INTO VoiceActivity(userid, channelid, timestamp, action ) VALUES(?, ?, ?, ?)",
+      [userid, channelid, seconds(), action])
+  }
+}
+
+module.exports = {
+  Actions : Actions,
+  Logger : Logger
+}
